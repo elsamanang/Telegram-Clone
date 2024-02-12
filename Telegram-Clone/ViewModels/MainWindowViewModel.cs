542a51af07f9064962c04bdffb4c4a2700e4e246
elsa.m <manangelsa07@gmail.com>
2024-02-12 16:06:28 +0100

    list of message

--- Telegram-Clone/ViewModels/MainWindowViewModel.cs
+++ Telegram-Clone/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using TelegramClone.Models;
+using TelegramClone.Utils;
 
 namespace Telegram_Clone.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Prism Application";
+        private ObservableCollection<Message> _messages;
         public string Title
         {
             get { return _title; }
@@ -13,7 +17,14 @@ namespace Telegram_Clone.ViewModels
 
         public MainWindowViewModel()
         {
+            var data = MockGenerator.GenerateMessages(15);
+            _messages = new ObservableCollection<Message>(data);
+        }
 
+        public ObservableCollection<Message> Messages
+        {
+            get => _messages;
+            set => SetProperty(ref _messages, value);   
         }
     }
 }
