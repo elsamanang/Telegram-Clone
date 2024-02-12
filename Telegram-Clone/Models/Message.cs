656e99a8e239366cdef9bb66adf43cea24ed846c
elsa.m <manangelsa07@gmail.com>
2024-02-12 16:05:27 +0100

    adding message modal

---
+++ Telegram-Clone/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramClone.Models
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPicture { get; set; }
+        public DateTime Time { get; set; }
+        public string Content { get; set; }
+    }
+}
