efc4b2cddf989837f1214c79e1bcb730b0527880
elsa.m <manangelsa07@gmail.com>
2024-02-12 16:06:02 +0100

    mocking data

---
+++ Telegram-Clone/Utils/MockGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramClone.Models;
+
+namespace TelegramClone.Utils
+{
+    public static class MockGenerator
+    {
+        public static IEnumerable<Message> GenerateMessages(int number)
+        {
+            var faker = new Faker<Message>()
+                .RuleFor(m => m.UserName, f => f.Person.UserName)
+                .RuleFor(m => m.UserPicture, f => f.Person.Avatar)
+                .RuleFor(m => m.Time, f => f.Date.Past(Random.Shared.Next(1, 5), DateTime.Now))
+                .RuleFor(m => m.Content, f => f.Lorem.Paragraph().Substring(0, 45) + "...");
+
+            var data = faker.Generate(number);
+
+            return data;
+        }
+    }
+}
