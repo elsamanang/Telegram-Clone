5e1cfa1b0d7ec75c796e80021de4bafab7b77ec4
elsa.m <manangelsa07@gmail.com>
2024-02-15 15:06:18 +0100

    update title bar

--- Telegram-Clone/Views/MainWindow.xaml.cs
+++ Telegram-Clone/Views/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using AdonisUI.Controls;
+using System.Windows;
 
 namespace Telegram_Clone.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AdonisWindow
     {
         public MainWindow()
         {
