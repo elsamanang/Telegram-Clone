2f07f60e95382b155e65b7949f0a37e0263b9fb5
elsa.m <manangelsa07@gmail.com>
2024-02-19 15:30:42 +0100

    adding config

---
+++ Telegram-Clone/ConfigModule.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramClone.Models;
+using TelegramClone.Views;
+
+namespace TelegramClone
+{
+    class ConfigModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
