1a8dc392fa6ecd016ae06ddab448c15ebc9a4b07
elsa.m <manangelsa07@gmail.com>
2024-02-19 15:30:56 +0100

    adding home page

---
+++ Telegram-Clone/ViewModels/HomeViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TelegramClone.Models;
+
+namespace TelegramClone.ViewModels
+{
+    class HomeViewModel : BindableBase, INavigationAware
+    {
+        private Message _message;
+
+        public HomeViewModel() 
+        { 
+           
+        }
+
+        public Message Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Message = (Message)navigationContext.Parameters["myUser"];
+        }
+    }
+}
