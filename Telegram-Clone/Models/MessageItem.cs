309e5c355628e2b8a37f1ff41399be8757af9365
elsa.m <manangelsa07@gmail.com>
2024-02-26 15:39:47 +0100

    new model

---
+++ Telegram-Clone/Models/MessageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramClone.Models
+{
+    public class MessageItem
+    {
+        public string Message { get; set; }
+        public string Heure { get; set; }
+        public string Orientation { get; set; }
+    }
+}
